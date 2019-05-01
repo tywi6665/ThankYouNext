15c716d75138e0abaee044a7a969956edb061b94
tywi6665 <tyletcole@gmail.com>
2019-05-01 11:32:02 -0600

    made simple app with routing

---
+++ .next/static/webpack/static/development/pages/index.js.f896aa41aedc4c267bfd.hot-update.js
@@ -0,0 +1,44 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _comps_Header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../comps/Header */ "./comps/Header.js");
+var _jsxFileName = "C:\\Users\\Tyler Winstead\\Desktop\\Code\\ThankYouNext\\pages\\index.js";
+
+
+function Index() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_comps_Header__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Hello Next.js"));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f896aa41aedc4c267bfd.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../comps/MyLayout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  )
+}
